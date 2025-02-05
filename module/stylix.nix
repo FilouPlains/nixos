65f5e7692956e1adac1d50360d70dc8821c27f50
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-02-05 17:42:56 +0100

    Trying to throw away wallpaper.

--- module/stylix.nix
+++ module/stylix.nix
@@ -9,7 +9,7 @@
   stylix.polarity = "dark";
 
   # Mandatory, even if not used…
-  stylix.image = "../img/wallpaper.png";
+  stylix.image = null;#"../img/wallpaper.png";
 
   # Specify default fonts.
   stylix.fonts = {
