80974db47dc19179a3527ddd5b280e08563c30c3
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-03-12 21:17:58 +0100

    Adding a new home-manager module.

---
+++ module/home_manager/keychain.nix
@@ -0,0 +1,17 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.keychain = {
+    enable = true;
+
+    keys = [
+       "github_id_rsa"
+    ];
+
+    agents = [ "ssh" ];
+
+    extraFlags = [
+      "--inherit any"
+      "--quiet"
+    ];
+}
