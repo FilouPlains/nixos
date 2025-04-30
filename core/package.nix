d9d8aab3ec64458e4214ce53b054e2d51c7e2e30
FilouPlains <lucas.rouaud@gmail.com>
2025-04-30 18:56:41 +0200

    Adding masterpiece 10on10 IGN.

--- core/package.nix
+++ core/package.nix
@@ -6,14 +6,100 @@
 let
   path = /etc/nixos;
 
-  # Get enable packages by this
-  enabledPackage = config.package.enable;
+  filterOutPackage = lib.attrNames (lib.filterAttrs (_: isEnable: !isEnable) config.enablePackage);
+  
+  selectedPackage = packageList: (lib.subtractLists filterOutPackage packageList) |>
+    builtins.map (packageName: lib.strings.splitString "." packageName) |>
+    builtins.map (packageNameList: getSubPackage pkgs packageNameList);
 
-  filterOutPackage = lib.attrNames (lib.filterAttrs (_: isEnable: !isEnable) mySet);
-  selectedPackage = package: lib.subtractLists (filterOutPackage enabledPackage) (package);
+  getSubPackage = (source: list:
+    if builtins.length list == 1 then
+      builtins.getAttr (builtins.elemAt list 0) (source)
+    else
+      getSubPackage (builtins.getAttr (builtins.elemAt list 0) (source)) (builtins.tail list)
+  );
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  defaultPackage = [
+    # B
+    "bat"
+    "btop"
+
+    # D
+    "discord"
+
+    # E
+    "exfat"
+    "exfatprogs"
+
+    # F
+    "fastfetch"
+    "fd"
+    "fish"
+    "fishPlugins.fzf"
+    "fishPlugins.grc"
+    "fzf"
+
+    # G
+    "g"
+    "gimp"
+    "git"
+    "grc"
+
+    # H
+    "htop"
+
+    # I
+    "imagemagick"
+    "inkscape"
+
+    # K
+    "keychain"
+    "kitty"
+
+    # L
+    "lazygit"
+    "libreoffice-qt6-fresh"
+    "libvlc"
+
+    # M
+    "mlocate"
+
+    # N
+    "nerdfonts"
+    "neovim"
+    "nixpkgs-fmt"
+    "nvimpager"
+
+    # P
+    "pulseaudio"
+
+    # S
+    "sl"
+    "scrcpy"
+    "starship"
+
+    # V
+    "vesktop"
+    "vlc"
+
+    # W
+    "wget"
+
+    # X
+    "xclip"
+    "xwaylandvideobridge"
+
+    # Y
+    "yazi"
+
+    # Z
+    "zoxide"
+  ];
 in
 {
-  options.package.enable = lib.mkOption {
+  options.enablePackage = lib.mkOption {
     type = lib.types.attrsOf lib.types.bool;
     default = {};
     description = "Enable or disable system packages.";
@@ -25,93 +111,7 @@ in
       g = pkgs.callPackage "${path}/package/g/default.nix" { };
     };
 
-    package = [
-      btop
-      discord
-      bat
-      g
-    ];
-
-    environment.systemPackages = with pkgs; selectedPackage (package);
-  }
-
-  # # List packages installed in system profile. To search, run:
-  # # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   # B
-  #   batv
-  #   btop
-
-  #   # D
-  #   discord
-
-  #   # E
-  #   exfat
-  #   exfatprogs
-
-  #   # F
-  #   fastfetch
-  #   fd
-  #   fish
-  #   fishPlugins.fzf
-  #   fishPlugins.grc
-  #   fzf
-
-  #   # G
-  #   g
-  #   gimp
-  #   git
-  #   grc
-
-  #   # H
-  #   htop
-
-  #   # I
-  #   imagemagick
-  #   inkscape
-
-  #   # K
-  #   keychain
-  #   kitty
-
-  #   # L
-  #   lazygit
-  #   libreoffice
-  #   libvlc
-
-  #   # M
-  #   mlocate
-
-  #   # N
-  #   nerdfonts
-  #   neovim
-  #   nixpkgs-fmt
-  #   nvimpager
-
-  #   # P
-  #   pulseaudio
-
-  #   # S
-  #   sl
-  #   scrcpy
-  #   starship
-
-  #   # V
-  #   vesktop
-  #   vlc
-
-  #   # W
-  #   wget
-
-  #   # X
-  #   xclip
-  #   xwaylandvideobridge
-
-  #   # Y
-  #   yazi
-
-  #   # Z
-  #   zoxide
-  # ];
+    environment.systemPackages = selectedPackage (defaultPackage);
+  };
 }
 
