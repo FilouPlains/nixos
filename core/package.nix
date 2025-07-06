bdbdc8af541f1bd093607b241e6eefe4e056d99d
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-07-06 12:31:38 +0200

    Adding docstring.

--- core/package.nix
+++ core/package.nix
@@ -6,36 +6,64 @@
 let
   path = /etc/nixos;
 
-  filterOutPackage = lib.attrNames (lib.filterAttrs (_: isEnable: !isEnable) config.enablePackage);
-  
-  /*selectedPackage = packageList: (lib.subtractLists filterOutPackage packageList) |>
-    builtins.map (packageName: lib.strings.splitString "." packageName) |>
-    builtins.map (packageNameList: getSubPackage pkgs packageNameList);
-*/
-  selectedPackage = packageList: (lib.subtractLists config.disabledPackage packageList) |>
-    builtins.map (packageName: lib.strings.splitString "." packageName) |>
-    builtins.map (packageNameList: getSubPackage pkgs packageNameList);
+  /** 
+    Recursively navigates a nested attribute set using a list of strings as keys.
+    
+    Example
+    -------
+    Here how will work the loop:
+    - **Input:** `source [ a b c ]`
+    
+    1. `source.a`     [ b c ]
+    2. `source.a.b`   [ b ]
+    3. `source.a.b.c`
 
+    Note that here, we transform a `str` like `"a.b.c"` into the `[ a b c ]` list.
+    
+    Parameters
+    ----------
+    source : `AttrSet`  
+        The attribute set to navigate.
+    
+    list : `listOf str`  
+        A list of strings representing the path of keys to traverse.
+    
+    Returns
+    -------
+    `Any`  
+        The value located at the end of the path within the attribute set.
+  */
   getSubPackage = (source: list:
     if builtins.length list == 1 then
       builtins.getAttr (builtins.elemAt list 0) (source)
     else
       getSubPackage (builtins.getAttr (builtins.elemAt list 0) (source)) (builtins.tail list)
   );
+
+  /** 
+    Selects enabled packages (filtering out those in `config.disabledPackage`), then
+    gets their values from the `pkgs` attribute.
+    
+    Parameters
+    ----------
+    packageList : `listOf str`  
+        A list of dot-separated package names (`"languages.python.numpy"`).
+    
+    Returns
+    -------
+    `listOf Any`  
+        A list of resolved package values from `pkgs`, excluding any disabled packages.
+  */
+  selectedPackage = packageList: (lib.subtractLists config.disabledPackage packageList) | >
+    builtins.map (packageName: lib.strings.splitString "." packageName) | >
+  builtins.map (packageNameList: getSubPackage pkgs packageNameList);
 in
 {
-  options.enablePackage = lib.mkOption {
-    type = lib.types.attrsOf lib.types.bool;
-    default = {};
-    description = "Enable or disable system packages.";
-  };
-
   options.disabledPackage = lib.mkOption {
     type = lib.types.listOf lib.types.str;
-    default = [];
+    default = [ ];
     description = "List of system packages to disable.";
   };
-
 
   config = {
     # Declare manually installed packages.
@@ -121,7 +149,7 @@ in
       # Z
       "zoom-us"
       "zoxide"
-  ]);
+    ]);
   };
 }
 
