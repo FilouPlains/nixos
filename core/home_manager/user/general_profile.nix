194ed973bf04e7e1ecf88790cbcfbe8f0913ef03
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-07-09 18:24:17 +0200

    Deleting quotes.

--- core/home_manager/user/general_profile.nix
+++ core/home_manager/user/general_profile.nix
@@ -3,7 +3,7 @@
 {
   imports =
     [
-      "../package_setup/package_setup.nix"
+      ../package_setup/package_setup.nix
     ];
   # Do not change this value.
   # If you want to do so, check `Home Manager release notes`.
--- core/home_manager/user/lucas.rouaud/lucas.rouaud.nix
+++ core/home_manager/user/lucas.rouaud/lucas.rouaud.nix
@@ -3,7 +3,7 @@
 {
   imports =
     [
-      "../general_profile.nix"
+      ../general_profile.nix
     ];
 
   # Home Manager needs a bit of information about you and the paths it should
