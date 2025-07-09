b3bf39da02df677fcf6cab93bc89a6b79247f647
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-07-09 15:17:48 +0200

    Factorize users.

---
+++ core/home_manager/user/general_profile.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      "../package_setup/package_setup.nix"
+    ];
+  # Do not change this value.
+  # If you want to do so, check `Home Manager release notes`.
+  home.stateVersion = "24.05";
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    MANROFFOPT "-P -c";
+    MANPAGER = "nvimpager";
+    PAGER = "nvimpager";
+    COLORTERM = "truecolor";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
