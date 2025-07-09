39f80adfe44df214a69a274a88717cf164ae9ac9
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-07-09 15:17:37 +0200

    Moving into user general profile.

--- core/home_manager/package_setup/package_setup.nix
+++ core/home_manager/package_setup/package_setup.nix
@@ -30,19 +30,4 @@
       # S
       terminal/starship.nix
     ];
-
-  # Do not change this value.
-  # If you want to do so, check `Home Manager release notes`.
-  home.stateVersion = "24.05";
-
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    MANROFFOPT "-P -c";
-    MANPAGER = "nvimpager";
-    PAGER = "nvimpager";
-    COLORTERM = "truecolor";
-  };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
