6aa34aa16f4d8dbd1cbdeaab9ec37266f81a93a1
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-07-14 20:39:54 +0200

    Adding more complex user management with filtering.

--- core/nix_asset/module/user/definition.nix
+++ core/nix_asset/module/user/definition.nix
@@ -1,10 +1,77 @@
 { lib, config, pkgs, ... }:
 
 let
-  cfg = config.users;
+  userToAdd = config.user.userToAdd;
+  definedUser = config.user.userList;
+
+  /**
+    Filters a list of user definitions to include.
+
+    Parameters
+    ----------
+    userToAdd : `lib.types.listOf lib.types.str`
+        A list of user names that should be retained for a given host.
+
+    definedUser : `lib.types.listOf lib.types.setType`
+        A list of defined user attribute in a set.
+
+    Returns
+    -------
+    `lib.types.listOf lib.types.setType`
+        A filtered list of user to include.
+   */
+  filteredUserToAdd = builtins.filter (definedUserItem: builtins.elem definedUserItem.name userToAdd) definedUser;
+
+  /**
+    Extract user names from the `definedUser` list.
+
+    Parameters
+    ----------
+    definedUser : `lib.types.listOf lib.types.setType`
+        A list of defined user attribute in a set.
+
+    Returns
+    -------
+    `lib.types.listOf lib.types.str`
+        A list of defined user names.
+   */
+  userName = map (definedUserItem: definedUserItem.name) definedUser;
+
+  /**
+    Filters a list of user definitions to check which are asked to be included, but
+    actually are not existing.
+
+    Parameters
+    ----------
+    userName : `lib.types.listOf lib.types.str`
+        A list of defined user names.
+
+    userToAdd : `lib.types.listOf lib.types.str`
+        A list of user names that should be retained for a given host.
+
+    Returns
+    -------
+    `lib.types.listOf lib.types.str`
+        A filtered list of user that are undefined.
+   */
+  undefinedUser = builtins.filter (userToAddItem: !builtins.elem userToAddItem userName) userToAdd;
 in
 {
-  options.users = {
+  /**
+    Throw an error when a user is asked to be added for a host, but is not definied
+    in `definedUser`.
+  */
+  if builtins.length undefinedUser != 0 then
+    abort (
+      builtins.concatStringsSep "" [
+        "[Err##] The following users in `user.userToAdd` do not exist in "
+	"`user.userList`:\n - "
+        (builtins.concatStringsSep "\n  - " undefinedUser)
+      ]
+    )
+  else ""
+
+  options.user = {
     userList = lib.mkOption {
       type = lib.types.listOf (lib.types.submodule {
         options = {
@@ -72,7 +139,7 @@ in
             initialPassword = "";
           };
         })
-        cfg.userList
+        filteredUserToAdd
     );
 
     home-manager.users = builtins.listToAttrs (
@@ -82,8 +149,7 @@ in
           value = import "../../../home_manager/user/" +
             "${user.name}/${user.name}.nix";
         })
-        cfg.userList
+        filteredUserToAdd
     );
   };
 }
-
