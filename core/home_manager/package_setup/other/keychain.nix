d5e491e5963a5d94ade8341e2ced379f69da5029
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-07-30 19:16:56 +0200

    Moving files to another directory.

---
+++ core/home_manager/package_setup/other/keychain.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, osConfig, ... }:
+
+{
+  programs.keychain = {
+    enable = !builtins.elem "keychain" osConfig.disabledPackage or true;
+
+    keys = [
+      "github_id_rsa"
+    ];
+
+    agents = [ "ssh" ];
+
+    extraFlags = [
+      "--inherit any"
+      "--quiet"
+    ];
+  };
+}
