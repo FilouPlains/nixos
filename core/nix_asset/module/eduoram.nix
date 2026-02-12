91682d4451d51062999a20d2550cddabd73f5a39
FilouPlains <lucas.rouaud@gmail.com>
2026-02-12 15:57:51 +0100

    Trying to setup eduoram…

---
+++ core/nix_asset/module/eduoram.nix
@@ -0,0 +1,36 @@
+_: {
+  networking.networkmanager.enable = true;
+
+  environment.etc."ssl/certs/eduroam-ca.pem".source =
+    /etc/ssl/certs/eduroam_ca.pem;
+
+  networking.networkmanager.ensureProfiles.profiles.eduroam = {
+    connection = {
+      id = "eduroam";
+      type = "wifi";
+      autoconnect-priority = -10;
+    };
+
+    wifi.ssid = "eduroam";
+    wifi-security.key-mgmt = "WPA-EAP";
+
+    "802-1x" = {
+      anonymous-identity = "anonymous@u-paris.fr";
+      ca-cert = "/etc/ssl/certs/eduroam-ca.pem";
+      eap = "TTLS";
+      identity = "lucas.rouaud1";
+      phase2-auth = "pap";
+
+      altsubject-matches = builtins.concatStringsSep "" [
+        "DNS:rad01.u-paris.fr;"
+        "DNS:rad02.u-paris.fr;"
+        "DNS:radcp.u-paris.fr"
+      ];
+
+      password-flags = 2;
+    };
+
+    ipv4.method = "auto";
+    ipv6.method = "auto";
+  };
+}
