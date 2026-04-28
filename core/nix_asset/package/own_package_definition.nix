4eee5ba6e125fc9483c585dc727821fcf3237aae
Lucas ROUAUD <lucas.rouaud@gmail.com>
2026-04-29 01:57:20 +0200

    Now g-ls is in NixOS.

--- core/nix_asset/package/own_package_definition.nix
+++ core/nix_asset/package/own_package_definition.nix
@@ -8,15 +8,5 @@
   path = /etc/nixos/core/nix_asset/package;
 in {
   nixpkgs.config.packageOverrides = pkgs: {
-    g = pkgs.callPackage "${path}/g/default.nix" {};
-    # nerd-fonts =
-    #   pkgs.nerd-fonts
-    #   // {
-    #     inconsolata = import "${path}/inconsolata/default.nix" {
-    #       inherit (pkgs) fetchzip;
-    #       inherit (pkgs) lib;
-    #       inherit (pkgs) nerd-fonts;
-    #     };
-    #   };
   };
 }
